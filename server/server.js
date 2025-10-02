961c764d97b228f012a5494d4a3e83109d75d5a5
LaxmiGodara <lgodara285@gmail.com>
2025-10-02 09:44:36 +0530

    feat: Connect to MongoDB and create Product model

--- server/server.js
+++ server/server.js
@@ -1,12 +1,30 @@
 
 require('dotenv').config();
 const express = require ('express');
+const mongoose = require('mongoose'); 
 
 
 const app = express();
 
 
+
 const PORT = process.env.PORT||8000;
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected successfully.');
+    
+    app.listen(PORT, () => {
+      console.log(`Server is running successfully on http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Database connection failed:', err);
+    process.exit(1); 
+  });
+
+
 
 
 
@@ -15,6 +33,3 @@ app.get("/", (req, res) => {
 });
 
 
-app.listen(PORT, ()=>{
-    console.log(`Server running at http://localhost:${PORT}`)
-});
